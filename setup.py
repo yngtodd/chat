d8dd8e5e8d17b41659b0855665d4a54832ce652d
yngtodd <young.todd.mk@gmail.com>
2020-04-21 10:40:57 -0400

    Clean up setup
    No longer including rst files in the long description.

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
-
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 
 setup_requirements = ['pytest-runner', ]
@@ -40,7 +36,7 @@ setup(
     entry_points={'console_scripts': COMMANDS},
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='chat',
     name='le_chat',
